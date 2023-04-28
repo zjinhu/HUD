504e548f3c9840c6e3122537f979fd5171f62869
jinhu <814030966@qq.com>
2023-04-28 12:13:36 +0800

    ceshi

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUI-HUD",
+    name: "HUD",
     platforms: [
       .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUI-HUD",
-            targets: ["SwiftUI-HUD"]),
+            name: "HUD",
+            targets: ["HUD"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftUI-HUD",
+            name: "HUD",
             dependencies: []),
     ]
 )
--- SwiftUIHUD.podspec
+++ SwiftUIHUD.podspec
@@ -1,17 +1,17 @@
 
 Pod::Spec.new do |s|
 s.name             = 'SwiftUIHUD'
-s.version          = '0.0.1'
+s.version          = '0.0.2'
 s.summary          = 'Loading组件.'
 
 s.description      = <<-DESC
-工具.
+			Loading组件
 DESC
 
 s.homepage         = 'https://github.com/jackiehu/'
 s.license          = { :type => 'MIT', :file => 'LICENSE' }
 s.author           = { 'HU' => '814030966@qq.com' }
-s.source           = { :git => 'https://github.com/jackiehu/SwiftUI-HUD', :tag => s.version.to_s }
+s.source           = { :git => 'https://github.com/jackiehu/HUD', :tag => s.version.to_s }
 
 s.ios.deployment_target = "14.0"
 s.swift_versions     = ['5.5','5.4','5.3','5.2','5.1','5.0']
